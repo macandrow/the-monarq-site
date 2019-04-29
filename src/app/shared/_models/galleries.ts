f97a7387f34f70d922bdd298caa894a12c4a7bd5
sf <macandrow@gmail.com>
2019-04-29 14:34:40 -0700

    Reordered texture on polaroids
    also removed decks and positioning for this gallery

--- src/app/shared/_models/galleries.ts
+++ src/app/shared/_models/galleries.ts
@@ -391,7 +391,7 @@ export const GALLERIES: IGallery[] = [
   {
     id: 1,
     title: 'photography',
-    description: 'I will be photography',
+    description: 'for images with no prints add link to request a print links to contact form. I will be photography',
     thumbnail: ' hello',
     isProject: true,
     slides: [
@@ -400,51 +400,6 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 0,
-        position: 0,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img112.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 0,
-        position: 1,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img113.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 1,
-        position: 1,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img114.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 1,
-        position: 0,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img116.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 2,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img117_2.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 2,
         album: 'texture-on-polaroid'
       },
       {
@@ -452,7 +407,6 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 3,
         album: 'texture-on-polaroid'
       },
       {
@@ -460,7 +414,6 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 3,
         album: 'texture-on-polaroid'
       },
       {
@@ -468,71 +421,13 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 3,
         album: 'texture-on-polaroid'
       },
       {
-        src: '/assets/images/texture_on_polaroid/img184.jpg',
+        src: '/assets/images/texture_on_polaroid/img116.jpg',
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 3,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img179.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 4,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img172.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 4,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img166.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 4,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img158.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 4,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img157.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 4,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img153.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 5,
-        album: 'texture-on-polaroid'
-      },
-      {
-        src: '/assets/images/texture_on_polaroid/img152.jpg',
-        w: 900,
-        h: 1067,
-        title: 'Texture on Polariod',
-        deck: 5,
         album: 'texture-on-polaroid'
       },
       {
@@ -540,7 +435,83 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 6,
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img113.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img114.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img117_2.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img184.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img179.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img166.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img172.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img112.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img158.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img153.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
+        album: 'texture-on-polaroid'
+      },
+      {
+        src: '/assets/images/texture_on_polaroid/img152.jpg',
+        w: 900,
+        h: 1067,
+        title: 'Texture on Polariod',
         album: 'texture-on-polaroid'
       },
       {
@@ -548,7 +519,6 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 6,
         album: 'texture-on-polaroid'
       },
       {
@@ -556,7 +526,6 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Texture on Polariod',
-        deck: 7,
         album: 'texture-on-polaroid'
       },
       {
@@ -564,87 +533,8 @@ export const GALLERIES: IGallery[] = [
         w: 900,
         h: 1067,
         title: 'Aura <a href="http://yahoo.com"> link </a> <p> other text </p>',
-        deck: 0,
         album: 'aura'
       },
-      {
-        src: 'https://media.gettyimages.com/photos/the-house-used-in-the-american-sitcom-the-brady-bunch-has-been-listed-picture-id1001904568',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 0,
-        position: 0,
-        album: 'at-home'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/modern-architecture-in-chile-from-raimundo-anguita-in-hacienda-picture-id976189812',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 1,
-        position: 0,
-        album: 'at-home'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/mother-and-father-watching-young-daughter-play-picture-id659856897',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 1,
-        position: 1,
-        album: 'at-home'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/newlyconstructed-home-is-offered-for-sale-on-april-24-2018-in-chicago-picture-id950869234',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 1,
-        position: 3,
-        album: 'at-home'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/dilapidated-haunted-house-in-maine-picture-id149267319',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 1,
-        position: 2,
-        album: 'at-home'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/autumn-in-bostons-brighton-neighborhood-picture-id1032330550',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 1,
-        position: 6,
-        album: 'at-home'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/passenger-train-is-seen-on-the-great-belt-bridge-after-a-railway-on-picture-id1076538784',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 0,
-        album: 'by-train'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/emegency-crews-work-at-the-scene-of-a-amtrak-train-derailment-on-18-picture-id894968984',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 0,
-        album: 'by-train'
-      },
-      {
-        src: 'https://media.gettyimages.com/photos/scenic-view-of-train-on-viaduct-in-switzerland-picture-id898687414',
-        w: 1200,
-        h: 900,
-        title: 'Image Caption',
-        deck: 0,
-        album: 'by-train'
-      }
     ]
   },
   {
