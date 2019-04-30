6562d4bed6106622834690081fabd00195a40656
sf <macandrow@gmail.com>
2019-04-30 16:13:43 -0700

    Gridded homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -46,48 +46,48 @@ export class HomeComponent implements OnInit, OnDestroy {
         maxGlare: .25,
       });
     });
-    $(window).scroll(function () {
+    // $(window).scroll(function () {
 
-      // selectors
-      const $window = $(window),
-        $body = $('body.home'),
-        $panel = $('.row');
+    //   // selectors
+    //   const $window = $(window),
+    //     $body = $('body.home'),
+    //     $panel = $('.row');
 
-      // Change 33% earlier than scroll position so colour is there when you arrive.
-      const scroll = $window.scrollTop() + ($window.height() / 5);
+    //   // Change 33% earlier than scroll position so colour is there when you arrive.
+    //   const scroll = $window.scrollTop() + ($window.height() / 5);
 
-      $panel.each(function () {
-        const $this = $(this);
+    //   $panel.each(function () {
+    //     const $this = $(this);
 
-        // if position is within range of this panel.
-        // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-        // Remember we set the scroll to 33% earlier in scroll const.
-        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+    //     // if position is within range of this panel.
+    //     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    //     // Remember we set the scroll to 33% earlier in scroll const.
+    //     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
 
-          // Remove all classes on body with color-
-          $body.removeClass(function (index, css) {
-            return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
-          });
+    //       // Remove all classes on body with color-
+    //       $body.removeClass(function (index, css) {
+    //         return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+    //       });
 
-          // Add class of currently active div
-          $body.addClass('color-' + $(this).data('color'));
-        } else if ($window.scrollTop() === 0) {
-          $body.removeClass(function (index, css) {
-            return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
-          });
-          $body.addClass('color-intro');
-        }
-      });
-    }).scroll();
+    //       // Add class of currently active div
+    //       $body.addClass('color-' + $(this).data('color'));
+    //     } else if ($window.scrollTop() === 0) {
+    //       $body.removeClass(function (index, css) {
+    //         return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+    //       });
+    //       $body.addClass('color-intro');
+    //     }
+    //   });
+    // }).scroll();
 
     // const body = document.getElementsByTagName('body')[0];
     // body.classList.add('auth');
   }
 
   ngOnDestroy(): void {
-    const body = document.getElementsByTagName('body')[0];
-    // body.classList.remove('color-fractures', 'color-photography', 'me');
-    body.removeAttribute('class');
+    // const body = document.getElementsByTagName('body')[0];
+    // // body.classList.remove('color-fractures', 'color-photography', 'me');
+    // body.removeAttribute('class');
   }	// onSelect(gallery: Gallery){
   // 	this.selectedGallery = gallery;
   // }
